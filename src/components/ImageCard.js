ff043c3c6b14bb6bf0c856ac98e58490ddfc56d0
osogrizz <lhtor@gmail.com>
2019-09-26 17:42:51 -0700

    Adds initial custom styles.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,17 +8,17 @@ const ImageCard = (props) => {
   return (
     !props.imgData 
     ? <h3>Loading</h3> 
-    : <Card raised="true" style={{ width: '45vw', margin: '80px auto' }} >
+    : <Card raised="true" style={{ width: '45vw', margin: '80px auto', background: '#424242' }} >
         <CardActionArea>
           <CardMedia >
               <img src={props.imgData.url} alt={props.imgData.title} style={{ width: '100%' }}/>
             </CardMedia> 
         </CardActionArea>
         <CardContent>
-          <Typography gutterBottom variant="h2" component="h2">{props.imgData.title}</Typography>
-          <Typography variant="body1" color="textSecondary" >{props.imgData.date}</Typography>
+          <Typography gutterBottom variant="h2" component="h2" style={{ color: '#fff' }}>{props.imgData.title}</Typography>
+          <Typography variant="body1" color="textSecondary" component="p" >{props.imgData.date}</Typography>
           <input type="date" onChange={props.handleDate}/>
-          <Typography variant="body2" color="textSecondary" >{props.imgData.explanation}</Typography>
+          <Typography variant="body2" color="textSecondary" component="p" style={{ color: '#999', padding: '40px' }}>{props.imgData.explanation}</Typography>
         </CardContent>
       </Card>   
   )
